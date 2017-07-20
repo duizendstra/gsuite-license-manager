94fdc0ec7422cfcee67b61aab9ad8cf029862358
Jasper Duizendstra <Jasper Duizendstra>
2017-07-20 18:18:40 +0200

    Added remove

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function gsuiteLicenseManager(mainSpecs) {
                 auth: auth,
                 productId: productId,
                 skuId: skuId,
-                userId: userId
+                resource: {
+                    "userId": userId
+                }
             };
 
             if (specs.fields) {
@@ -47,9 +49,50 @@ function gsuiteLicenseManager(mainSpecs) {
         });
     }
 
+    function remove(specs) {
+        return new Promise(function (resolve, reject) {
+            var productId = specs.productId;
+            var skuId = specs.skuId;
+            var userId = specs.userId;
+            var request = {
+                auth: auth,
+                productId: productId,
+                skuId: skuId,
+                userId: userId
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            var operation = retry.operation({
+                retries: 5,
+                factor: 3,
+                minTimeout: 1 * 1000,
+                maxTimeout: 60 * 1000,
+                randomize: true
+            });
+
+            operation.attempt(function () {
+                service.licenseAssignments.delete(request, function (err, response) {
+                    if (operation.retry(err)) {
+                        console.log("Warning, error %s occured, retry %d", err.code, operation.attempts());
+                        return;
+                    }
+                    if (err) {
+                        reject(operation.mainError());
+                        return;
+                    }
+                    resolve(response);
+                });
+            });
+        });
+    }
+
     auth = mainSpecs.auth;
     return {
-        insert: insert
+        insert: insert,
+        remove: remove
     };
 }
 
